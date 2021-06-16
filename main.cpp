635eb06d811dbfc73d4b526599f1bcc7ba93e959
pdegioanni <pdegioanni@mi.unc.edu.ar>
2021-06-16 18:34:17 -0300

    Se agrega clase vertice y funciones para crearlos a partir de las coordenas de la matriz filtrada

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <bitset>
-#include <typeinfo>
+//#include <typeinfo>
+#include <list>
+#include <iterator>
 #include <fstream>
 
 #define C 28
@@ -13,7 +15,8 @@
 
 using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
 
 //CLASE MATRIZ DE BITSETS
 class Matriz { 
@@ -51,16 +54,50 @@ void Matriz::setRow(int f, bitset<C> r){
 	M[f] = r;
 }
 
-
+//ESTRUCTURAS
 typedef struct{
 	Matriz m;//Malezas
 	Matriz b; //Barreras
-} data;
+} DATA;
+
+typedef struct{
+	int x;//Coordenada x
+	int y; //Coordenada y
+} POS;
+
+class VERTICE { 
+	private:
+		int n;
+		POS p;
+		list<int[2]>* vecinos; //Arreglo que almacenara nodos vecinos y las distancias ([n][dist])
+	public:
+		VERTICE(int n, int x, int y){
+			this -> n = n;
+			this -> p.x = x;
+			this -> p.y = y;
+		}
+		void printVertice();
+		
+};
+
+void VERTICE :: printVertice(){
+
+   cout <<"Vertice "<< this -> n
+   		<<"\t Coordenadas: ("<<this -> p.x << ", "<< this -> p.y <<")"<<endl;
+}
+
+/*typedef struct{
+	int n; //Numero de vertice
+	POS p; //Coordenadas
+	list<int[2]>* vecinos; //Arreglo que almacenara nodos vecinos y las distancias ([n][dist])
+} VERTICE;*/
 
 //DECLARACION DE FUNCIONES 
-data cargarMatriz();
+DATA cargarMatriz();
 Matriz filtrarMatriz(Matriz m, int tamanoFiltro);
-void crearVertices();
+void generarVertices(list<VERTICE*>* vertices, Matriz m);
+//VERTICE crearVertice(int n, int x, int y);
+void printVertices(list<VERTICE*>* l);
 //void enlazarVerticesAlcanzables();
 void corroborarCamino();
 void calcularDistanciaEuclidiana();
@@ -79,20 +116,35 @@ void definirCircuito();
 int main(int argc, char** argv) {
 	
 	//1) Carga de datos desde el archivo
-	data entrada = cargarMatriz();
+	DATA d = cargarMatriz();
 	cout<< "Matriz de malezas: "<<endl;
-	entrada.m.print();
+	d.m.print();
 	cout<<endl<< "Matriz de barreras: "<<endl;
-	entrada.b.print();
+	d.b.print();
 	
-	//2)Filtrado de manchas significativas
-	filtrarMatriz(entrada.m, 3);
-	return 0;
+	//2)Filtrado de manchas significativas. Si la mancha es mayor al tamano del filtro quedan 1s consecutivos
+	d.m = filtrarMatriz(d.m, 3);
+	cout<< endl<<"Matriz de malezas filtrada: "<<endl;
+	d.m.print();
+	
+	
+	//3)Creacion de un vertice del grafo por cada mancha significativa. 
+	//Si la mancha es mayor al filtro aplicado, se la reduce al punto correspondiente a su esquina superior izquierda
+	//Se registran las coordenadas de cada vertice.
+	list<VERTICE*> vv;
+	list<VERTICE*>* vertices = &vv;
+	generarVertices(vertices, d.m);
+	printVertices(vertices);
+	//cout<< endl<<"Coordenadas manchas significativas: "<<endl;
+	//d.m.print();
+	
+	
+	return 0;	
 }
 
 //CargarMatriz() -> genera las matrices de entrada correspondientes a las malezas y a las barreras
-data cargarMatriz(){
-	data d;
+DATA cargarMatriz(){
+	DATA d;
 
 	ifstream f("img.txt");
 	string line;
@@ -122,8 +174,8 @@ Matriz filtrarMatriz(Matriz m, int tamanoFiltro){
 	bool submatriz[tamanoFiltro][tamanoFiltro];
 	
 	int cant = 0;
-	int cantVertices = 0;
 	
+	//Se aplica filtro cuadrado del tamano definido por parametro
 	for(int i=0;i<F;i++){
         for(int j=C-1;j>=0;j--) {
         	
@@ -143,20 +195,36 @@ Matriz filtrarMatriz(Matriz m, int tamanoFiltro){
 			
     }
     
-    s.print();
-    bool flag = false;
+    return s;
+	
+}
+
+void generarVertices(list<VERTICE*>* vertices, Matriz m){
+	Matriz s = m;
+	//list<VERTICE> vertices;
+	//VERTICE v;
+	int nVertice = 0;
+	bool flag = false;
+	
+	//s.print();
+	
     for(int i=0;i<F;i++){
         for(int j=C-1;j>=0;j--) {
-			if(s.getBit(i, j) == 1 && flag==false){
+			if(s.getBit(i, j) == 1 && flag==false){ 
 				flag = true;
-				cantVertices++;
+				nVertice++;
+				//cout<<"v "<<nVertice<<endl;
+				VERTICE* v = new VERTICE (nVertice, i+1, C-j);
+				//v.printVertice();
+				//VERTICE* vp = &v;
+				vertices -> push_back(v);
+				//printVertices(vertices);
+				//cout<<"(" << i+1<<", "<< C - j <<")"<<endl; //Coordenadas del vertice
 			}
 			else if(s.getBit(i, j) == 1 && flag==true){
 				s.clearBit(i,j);//Limpia el de la derecha
 				s.clearBit(i+1,j);//Limpia el de la derecha abajo
 				s.clearBit(i+1,j+1);//Limpia el de abajo
-				//s.clearBit(i-1,j);
-				//s.clearBit(i,j);
 				flag = false;
 			}
 			else{//if(s.getBit(i, j) == 0){
@@ -164,12 +232,39 @@ Matriz filtrarMatriz(Matriz m, int tamanoFiltro){
 			}
 		}
 	}
-    
-    s.print();
-    cout<<"Cantidad de vertices: "<<cantVertices<<endl;
-    
-    return s;
 	
+	cout<< endl<<"Coordenadas manchas significativas: "<<endl;
+	s.print();
+	//printVertices(vertices);
+    
+    //return vertices;
 }
+
+void printVertices(list <VERTICE*>* l){
+	list <VERTICE*> :: iterator it;
+    for(it = l -> begin(); it != l -> end(); ++it){
+   //for(it = l.begin(); it != l.end(); ++it){
+   	//cout << '\t' << it -> VERTICE -> p.x;
+        (*it) -> printVertice();
+    	cout << '\n';
+   }
+}
+
+/*VERTICE crearVertice(int n, int x, int y){
+	VERTICE v;
+	//VERTICE* ad = &v;
+	//ad -> n = n;
+	//ad -> p.x = x;
+	//ad -> p.y= y;
+	//ad -> vecinos;
+	cout<<"dentro de "<< n<<endl;
+	v.n = n;
+	v.p.x = x;
+	v.p.y= y;
+	v.vecinos;
+	return v;
+}*/
+
+
 
 
