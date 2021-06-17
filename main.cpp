6f055856e888fef2feaf9652b3da1f6c6454eba1
pdegioanni <pdegioanni@mi.unc.edu.ar>
2021-06-16 21:22:24 -0300

    Se agrega funcion para enlazar cada vertice con los vecinos alcanzables (sin barrera de por medio)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <bitset>
+#include <math.h>
 //#include <typeinfo>
 #include <list>
 #include <iterator>
@@ -65,25 +66,49 @@ typedef struct{
 	int y; //Coordenada y
 } POS;
 
-class VERTICE { 
+typedef struct{
+	int nVertice;//Coordenada x
+	double dist; //Coordenada y
+} VECINO;
+
+
+class Vertice { 
 	private:
 		int n;
 		POS p;
-		list<int[2]>* vecinos; //Arreglo que almacenara nodos vecinos y las distancias ([n][dist])
+		list<VECINO> vecinos; //Arreglo que almacenara nodos vecinos y las distancias ([n][dist])
 	public:
-		VERTICE(int n, int x, int y){
+		Vertice(int n, int x, int y){
 			this -> n = n;
 			this -> p.x = x;
 			this -> p.y = y;
 		}
+		int getNumero(){return n;};
+		//list<VECINO>* getVecinos(){return vecinos;};
+		POS getCoordenadas() {return p;};
 		void printVertice();
+		void printVecinos();
+		void addVecino(VECINO v);
 		
 };
 
-void VERTICE :: printVertice(){
+void Vertice :: printVertice(){
 
-   cout <<"Vertice "<< this -> n
-   		<<"\t Coordenadas: ("<<this -> p.x << ", "<< this -> p.y <<")"<<endl;
+   cout <<"Vertice "<< this -> n<<endl
+   		<<"\tCoordenadas: ("<<this -> p.x << ", "<< this -> p.y <<")"<<endl
+   		<<"\tVertices alcanzables:"<<endl;
+		printVecinos();
+}
+
+void Vertice :: addVecino(VECINO v){
+	this -> vecinos.push_back(v);
+}
+
+void Vertice :: printVecinos(){
+	list <VECINO> :: iterator it;
+	for(it = vecinos.begin(); it != vecinos.end(); ++it){
+		cout<<"\t\t<Vertice "<< it -> nVertice << ", distancia "<< it -> dist<<">"<< endl;
+	}
 }
 
 /*typedef struct{
@@ -95,22 +120,14 @@ void VERTICE :: printVertice(){
 //DECLARACION DE FUNCIONES 
 DATA cargarMatriz();
 Matriz filtrarMatriz(Matriz m, int tamanoFiltro);
-void generarVertices(list<VERTICE*>* vertices, Matriz m);
-//VERTICE crearVertice(int n, int x, int y);
-void printVertices(list<VERTICE*>* l);
-//void enlazarVerticesAlcanzables();
+Matriz generarVertices(list<Vertice*>* vertices, Matriz m);
+void printVertices(list<Vertice*>* l);
+void enlazarVerticesAlcanzables(list<Vertice*>* vertices, Matriz b);
 void corroborarCamino();
-void calcularDistanciaEuclidiana();
+double calcularDistancia(Vertice* v1, Vertice* v2);
 void definirCircuito();
 
-/*Matriz subMatriz(int finicio, int cinicio, int ffin, int cfin){
-	//Tamano de la submatriz
-	int c = cfin-cinicio;
-	int f = ffin - finicio;
-	
-	bitset<c> S[f];
-		
-}*/
+
 
 //CLASE PRINCIPAL
 int main(int argc, char** argv) {
@@ -131,10 +148,19 @@ int main(int argc, char** argv) {
 	//3)Creacion de un vertice del grafo por cada mancha significativa. 
 	//Si la mancha es mayor al filtro aplicado, se la reduce al punto correspondiente a su esquina superior izquierda
 	//Se registran las coordenadas de cada vertice.
-	list<VERTICE*> vv;
-	list<VERTICE*>* vertices = &vv;
-	generarVertices(vertices, d.m);
+	list<Vertice*> v;
+	list<Vertice*>* vertices = &v;
+	d.m = generarVertices(vertices, d.m);
+	cout<< endl<<"Ubicacion de las manchas significativas: "<<endl;
+	d.m.print();
+	cout<<endl;
+	cout<<endl<<"Vertices del grafo sin enlazar: "<<endl;
 	printVertices(vertices);
+	
+	//4)Enlazado de vertices: para cada uno se agregan los vertices alcanzables y su distancia
+	cout<<endl<<"Vertices del grafo enlazados: "<<endl;
+	enlazarVerticesAlcanzables(vertices, d.b);
+	
 	//cout<< endl<<"Coordenadas manchas significativas: "<<endl;
 	//d.m.print();
 	
@@ -154,7 +180,7 @@ DATA cargarMatriz(){
 	
 		for(int c = 0; c<line.size(); c++){
 			if(line[c]== 'x'){
-				d.b.setBit(i,c);//Se agrega a matriz de barreras
+				d.b.setBit(i,line.size()-c);//Se agrega a matriz de barreras
 				line[c] = '0';//Se elimina de matriz de malezas	
 			} 
 			
@@ -199,27 +225,18 @@ Matriz filtrarMatriz(Matriz m, int tamanoFiltro){
 	
 }
 
-void generarVertices(list<VERTICE*>* vertices, Matriz m){
+Matriz generarVertices(list<Vertice*>* vertices, Matriz m){
 	Matriz s = m;
-	//list<VERTICE> vertices;
-	//VERTICE v;
 	int nVertice = 0;
 	bool flag = false;
-	
-	//s.print();
 	
     for(int i=0;i<F;i++){
         for(int j=C-1;j>=0;j--) {
 			if(s.getBit(i, j) == 1 && flag==false){ 
 				flag = true;
 				nVertice++;
-				//cout<<"v "<<nVertice<<endl;
-				VERTICE* v = new VERTICE (nVertice, i+1, C-j);
-				//v.printVertice();
-				//VERTICE* vp = &v;
-				vertices -> push_back(v);
-				//printVertices(vertices);
-				//cout<<"(" << i+1<<", "<< C - j <<")"<<endl; //Coordenadas del vertice
+				Vertice* v = new Vertice (nVertice, i+1, C-j); //Se crea un nuevo vertice con las coordenadas segun su ubicacion en la matriz
+				vertices -> push_back(v); //Se lo agrega a la lista de vertices
 			}
 			else if(s.getBit(i, j) == 1 && flag==true){
 				s.clearBit(i,j);//Limpia el de la derecha
@@ -227,44 +244,119 @@ void generarVertices(list<VERTICE*>* vertices, Matriz m){
 				s.clearBit(i+1,j+1);//Limpia el de abajo
 				flag = false;
 			}
-			else{//if(s.getBit(i, j) == 0){
+			else{//if(s.getBit(i, j) == 0)
 				flag = false;
 			}
 		}
 	}
 	
-	cout<< endl<<"Coordenadas manchas significativas: "<<endl;
-	s.print();
-	//printVertices(vertices);
-    
-    //return vertices;
+	return s;
 }
 
-void printVertices(list <VERTICE*>* l){
-	list <VERTICE*> :: iterator it;
+void printVertices(list <Vertice*>* l){
+	list <Vertice*> :: iterator it;
     for(it = l -> begin(); it != l -> end(); ++it){
-   //for(it = l.begin(); it != l.end(); ++it){
-   	//cout << '\t' << it -> VERTICE -> p.x;
         (*it) -> printVertice();
     	cout << '\n';
-   }
+    }
 }
 
-/*VERTICE crearVertice(int n, int x, int y){
-	VERTICE v;
-	//VERTICE* ad = &v;
-	//ad -> n = n;
-	//ad -> p.x = x;
-	//ad -> p.y= y;
-	//ad -> vecinos;
-	cout<<"dentro de "<< n<<endl;
-	v.n = n;
-	v.p.x = x;
-	v.p.y= y;
-	v.vecinos;
-	return v;
-}*/
+void enlazarVerticesAlcanzables(list <Vertice*>* vertices, Matriz b){
+	POS coord;
+	POS coordVecino;
+	
+	POS inicio;
+	POS final;
+	
+	VECINO nuevoVecino;
+	
+	bool nodoEnlazable = true;
+	double distancia;
+	
+	Vertice* v1 = vertices -> front();
+	vertices -> pop_front();
+	//cout<<"Vertice "<< v1 -> getNumero()<<endl;
+	
+	Vertice* v2;
+	
+	
+	list <Vertice*> :: iterator it;
+	while(!vertices -> empty()){
+	
+	    for(it = vertices -> begin(); it != vertices -> end(); ++it){
+				v2 = (*it);
+				coord = v1 -> getCoordenadas();
+				coordVecino = v2 -> getCoordenadas();
+				inicio = coord;
+				final = coordVecino;
+				
+				if(coord.x > coordVecino.x){
+					inicio.x = coordVecino.x;
+					final.x = coord.x;	
+				} 
+				if(coord.y > coordVecino.y){
+					inicio.y = coordVecino.y;
+					final.y = coord.y;	
+				} 
+				
+				
+				for(int i = inicio.x -1; i<= final.x-1; i++){
+					//cout<<endl;
+					//if(v1 -> getNumero() == 5 && v2 -> getNumero() ==6) cout<<"i "<<i <<endl;
+					for(int j = final.y-1; j>=inicio.y-1; j--){
+					//	if(v1 -> getNumero() == 5 && v2 -> getNumero() ==6) cout<<"i "<<i <<" j "<<j<<endl;
+						if(b.getBit(i, C-j) == 1) {
+							nodoEnlazable = false;
+							break;
+						}
+						
+					}
+				}
+				
+				if(nodoEnlazable){
+					//cout<< "Vertice " << v1 -> getNumero() << " es enlazable con vertice "<< v2 -> getNumero()<<endl;
+					
+					distancia = calcularDistancia(v1, v2);
+					//cout<<"Distancia: "<< distancia<<endl;
+					
+					VECINO nuevoVecino;
+					nuevoVecino.nVertice = v2 -> getNumero();
+					nuevoVecino.dist = distancia;
+					
+					//cout<<"Fffff "<<nuevoVecino.nVertice<<"     "<< nuevoVecino.dist<<endl;
+					//list<VECINO>* f = 
+					v1 -> addVecino(nuevoVecino);
+					
+					
+					// f -> push_front(nuevoVecino);
+					//cout<<"ddddd"<<endl;
+					
+					nuevoVecino.nVertice = v1 -> getNumero();
+					//v2 -> getVecinos() -> push_front(nuevoVecino);
+					v2 -> addVecino(nuevoVecino);
+				}
+				
+				nodoEnlazable = true;
+	    }
+	    v1 -> printVertice();
+	    v1 = vertices -> front();
+		vertices -> pop_front();
+		//cout<<"Vertice "<< v1 -> getNumero()<<endl;
+	}
+	v1 -> printVertice();
+}
 
+double calcularDistancia(Vertice* v1, Vertice* v2){
+	double resultado;
+	int x1 = v1 -> getCoordenadas().x;
+	int x2 = v2 -> getCoordenadas().x;
+	int y1 = v1 -> getCoordenadas().y;
+	int y2 = v2 -> getCoordenadas().y;
+	
+	resultado = sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
+	
+	return resultado;
+}
 
 
 
